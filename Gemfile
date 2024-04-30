e0bc9e73d3050c693c14d193b50f9609541e3f3a
Alexandre Machado <alexandre.machcruz@gmail.com>
2024-04-30 17:51:08 -0300

    Add jwt authorization

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem "bootsnap", require: false
 
 gem 'active_model_serializers'
 gem 'sneakers'
+gem 'jwt'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  before_action :authorize
+
+  private
+
+  def authorize
+    token = request.headers['Authorization']&.split(' ')&.last
+    JWT.decode(token, ENV['JWT_SECRET'], true, algorithm: 'HS256')
+  rescue JWT::DecodeError => e
+    render json: { errors: [{ message: e.message }] }, status: 401
+  end
 end
